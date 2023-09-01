2a0899ecec5a34ec4c3c4b360dba804b53e42d0c
Gustavo Friol <88865860+gustavofriol@users.noreply.github.com>
2023-09-01 16:58:36 -0300

    Update sql_automation.py

--- sql_automation.py
+++ sql_automation.py
@@ -6,9 +6,10 @@ from email.mime.base import MIMEBase
 from email import encoders
 from datetime import datetime, timedelta
 
-# Código de consulta SQL de cada usina
+# Definindo variáveis
+# Consultas:
 
-uis_query = """
+uis = """
 select Data, UIS_TOT from (
     select to_char(trunc(E3TIMESTAMP, 'hh24'), 'dd/mm/yyyy hh24:mi') as Data, 
            round(AVG(US_SCT_P_TOT), 2) as UIS_TOT 
@@ -18,7 +19,7 @@ select Data, UIS_TOT from (
     group by trunc(E3TIMESTAMP, 'hh24')
 )
 """
-uso_query = """
+uso = """
 select Data, USO_TOT from (
     select to_char(trunc(E3TIMESTAMP, 'hh24'), 'dd/mm/yyyy hh24:mi') as Data, 
            round(AVG(US_SCT_P_TOT),2) as USO_TOT 
@@ -28,17 +29,48 @@ select Data, USO_TOT from (
     group by trunc(E3TIMESTAMP, 'hh24')
 )
 """
-ucr_query = """
-select Data, UCR_TOT from (
+ucr_u1 = """
+select Data, round(UCR_U1_MW, 2) as UCR_U1_MW from (
     select to_char(trunc(E3TIMESTAMP, 'hh24'), 'dd/mm/yyyy hh24:mi') as Data, 
-           round(AVG(LT_SCT_P_TOT),2) as UCR_TOT 
-    from HT_UCR_SE
+           round(AVG(GE_SCT_P_TOT) / 1000, 2) as UCR_U1_MW 
+    from HT_UCR_U1
+    where to_char(E3TIMESTAMP, 'MMYYYY') = to_char(SYSDATE - INTERVAL '1' HOUR, 'MMYYYY') 
+      and trunc(E3TIMESTAMP, 'hh24') = trunc(SYSDATE - interval '1' hour, 'hh24') 
+    group by trunc(E3TIMESTAMP, 'hh24')
+)
+
+"""
+ucr_u2 = """
+select Data, round(UCR_U2_MW, 2) as UCR_U2_MW from (
+    select to_char(trunc(E3TIMESTAMP, 'hh24'), 'dd/mm/yyyy hh24:mi') as Data, 
+           round(AVG(GE_SCT_P_TOT) / 1000, 2) as UCR_U2_MW 
+    from HT_UCR_U2
     where to_char(E3TIMESTAMP, 'MMYYYY') = to_char(SYSDATE - INTERVAL '1' HOUR, 'MMYYYY') 
       and trunc(E3TIMESTAMP, 'hh24') = trunc(SYSDATE - interval '1' hour, 'hh24') 
     group by trunc(E3TIMESTAMP, 'hh24')
 )
 """
-urc_query = """
+ucr_u3 = """
+select Data, round(UCR_U3_MW, 2) as UCR_U3_MW from (
+    select to_char(trunc(E3TIMESTAMP, 'hh24'), 'dd/mm/yyyy hh24:mi') as Data, 
+           round(AVG(GE_SCT_P_TOT) / 1000, 2) as UCR_U3_MW 
+    from HT_UCR_U3
+    where to_char(E3TIMESTAMP, 'MMYYYY') = to_char(SYSDATE - INTERVAL '1' HOUR, 'MMYYYY') 
+      and trunc(E3TIMESTAMP, 'hh24') = trunc(SYSDATE - interval '1' hour, 'hh24') 
+    group by trunc(E3TIMESTAMP, 'hh24')
+)
+"""
+ucr_u4 = """
+select Data, round(UCR_U4_MW, 2) as UCR_U4_MW from (
+    select to_char(trunc(E3TIMESTAMP, 'hh24'), 'dd/mm/yyyy hh24:mi') as Data, 
+           round(AVG(GE_SCT_P_TOT) / 1000, 2) as UCR_U4_MW 
+    from HT_UCR_U4
+    where to_char(E3TIMESTAMP, 'MMYYYY') = to_char(SYSDATE - INTERVAL '1' HOUR, 'MMYYYY') 
+      and trunc(E3TIMESTAMP, 'hh24') = trunc(SYSDATE - interval '1' hour, 'hh24') 
+    group by trunc(E3TIMESTAMP, 'hh24')
+)
+"""
+urc = """
 select Data, URC_TOT from (
     select to_char(trunc(E3TIMESTAMP, 'hh24'), 'dd/mm/yyyy hh24:mi') as Data, 
            round(AVG(US_SCT_P_TOT),2) as URC_TOT 
@@ -49,27 +81,24 @@ select Data, URC_TOT from (
 )
 """
 
-# Definição e formatação das variáveis de data
-
 previous_time = datetime.now() - timedelta(hours=1)
+
 file_date = previous_time.strftime("%d_%m_%Y - %H_00")
 excel_date = previous_time.strftime("%d/%m/%Y  %H:00")
 
 # Configurando o log com rotação por número de arquivos
-
 log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-log_handler = RotatingFileHandler(r'K:\Geracao Comum\DVOP\Controle de Projetos\Inovvo\Histórico.log', maxBytes=1e6, backupCount=5)
+log_handler = RotatingFileHandler(r'K:\Geracao Comum\DVOP\Projetos\Inovvo\Histórico de erros.log', maxBytes=1e6, backupCount=5)
 log_handler.setFormatter(log_formatter)
 logger = logging.getLogger(__name__)
 logger.addHandler(log_handler)
 logger.setLevel(logging.DEBUG)
 
 
+# Definindo Funções
+
 def query(sql_query):
-    
-    # Definição das credenciais do banco de dados em que estão armazenados os dados de potência de cada usina (Informações ocultadas)
-    
-    conn = oracledb.connect(user= # usuário do BD # , password= # senha do BD #, dsn= # dsn do BD #)
+    conn = oracledb.connect(user='cog', password='ro5iww8b', dsn='jira ')
     try:
         cursor = conn.cursor()
         cursor.execute(sql_query)
@@ -86,7 +115,7 @@ def save_query():
     # Criar um novo arquivo Excel e selecionar a primeira planilha
     workbook = openpyxl.Workbook()
     sheet = workbook.active
-    column_a = [83669000, 73694000, 83740000, 90000050 # Nessa coluna são passados os códigos de cada usina
+    column_a = [83669000, 73694000, 83740000, 90000050]
     column_b = [excel_date] * len(column_a)
 
     # Preencher as colunas A e B com os valores
@@ -95,24 +124,25 @@ def save_query():
         sheet.cell(row=idx + 1, column=2, value=value_b)
 
     # Preencher coluna C com valores das potências de cada usina
-    sheet['C1'] = query(urc_query)
-    sheet['C2'] = query(ucr_query)
-    sheet['C3'] = query(uso_query)
-    sheet['C4'] = query(uis_query)
+    sheet['C1'] = query(urc)
+    sheet['C2'] = query(ucr_u1) + query(ucr_u2)
+    sheet['C3'] = query(uso)
+    sheet['C4'] = query(uis)
+    sheet['D2'] = query(ucr_u3) + query(ucr_u4)
 
     # Ajustar o tamanho da coluna B
     sheet.column_dimensions['B'].width = 16
 
     # Salvar o arquivo Excel com o nome fornecido
-    workbook.save(os.path.join(fr"C:\Relatórios Inovvo\Relatórios", f"Relatório Usinas - {file_date}.xlsx"))
+    workbook.save(os.path.join(fr"K:\Geracao Comum\DVOP\Projetos\Inovvo\Relatórios", f"Relatório Usinas - {file_date}.xlsx"))
 
 
 def send_query():
-    # Configurações do servidor SMTP da CELESC (Informações ocultadas)
-    smtp_host = # servidor smtp CELESC #  
-    smtp_port = # porta do servidor #
-    username = # email de quem envia #
-    receiver = # email de quem recebe #
+    # Configurações do servidor SMTP do Outlook
+    smtp_host = 'smtp.celesc.com.br'
+    smtp_port = 25
+    username = 'cog@celesc.com.br'
+    receiver = 'dados@inovvodata.com.br'
 
     # Criando o objeto MIMEMultipart para compor o e-mail
     msg = MIMEMultipart()
@@ -126,16 +156,16 @@ def send_query():
     msg.attach(MIMEText(email_body, 'plain'))
 
     # Caminho absoluto para o arquivo que será anexado ao e-mail
-    file_path = fr"C:\Relatórios Inovvo\Relatórios\Relatório Usinas - {file_date}.xlsx"
+    file_path = fr"K:\Geracao Comum\DVOP\Projetos\Inovvo\Relatórios\Relatório Usinas - {file_date}.xlsx"
 
     # Adicionando o caminho absoluto à lista de arquivos anexos
     files = [file_path]
     for file in files:
-        with open(file, 'rb') as f:
-            part = MIMEBase('application', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            part.set_payload(f.read())
+        with open(file, 'rb') as attachment:
+            part = MIMEBase('application', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', name=f"Relatório Usinas - {file_date}.xlsx")
+            part.set_payload(attachment.read())
             encoders.encode_base64(part)
-            part.add_header('Content-Disposition', f'attachment; filename= {os.path.basename(file)}')
+            part.add_header('Content-Disposition', 'attachment')
             msg.attach(part)
 
     # Conectando ao servidor SMTP e enviando o e-mail
@@ -143,7 +173,7 @@ def send_query():
             server.set_debuglevel(1)
             server.sendmail(username, msg['To'], msg.as_string())
             server.quit()
-
+ 
 
 # Chamando as funções e registrando erros, se houver
 
